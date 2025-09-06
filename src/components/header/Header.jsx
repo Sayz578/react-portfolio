2e35557bffe31f5381dbfaf0b2b12130d499af86
Sayz578 <kamaevyaroslav8@gmail.com>
2025-09-06 13:19:15 +0300

    commit

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,4 +1,4 @@
-import React,{useEffect, useState} from 'react'
+import React, { useEffect, useState } from 'react'
 import "./header.css"
 import logo from "../../assets/logo.png"
 import Nav from '../nav/Nav';
@@ -7,72 +7,78 @@ import { MdOutlineMenu } from "react-icons/md";
 import "./header.css"
 import { socialLinks } from '../../utils/socialIcons';
 import { useLocation } from 'react-router';
+import HeaderBottomPage from './HeaderBottomPage';
 
 
 const headerClasses = {
-  "/": "header",
-  "/about": "header__about",
-  "/service": "header__service",
-  "/portfolio": "header__portfolio",
+    "/": "header",
+    "/about": "header__about",
+    "/service": "header__service",
+    "/portfolio": "header__portfolio",
 };
 
 const Header = () => {
-    const location = useLocation(); 
+    const location = useLocation();
     const headerClass = headerClasses[location.pathname] || "header";
 
     const [isActive, setIsActive] = useState(false)
-     const handleScroll = () => {
-      if (window.scrollY >= 5) {
-        setIsActive(true);
-      } else {
-        setIsActive(false);
-      }
+    const handleScroll = () => {
+        if (window.scrollY >= 5) {
+            setIsActive(true);
+        } else {
+            setIsActive(false);
+        }
     };
 
     useEffect(() => {
-       window.addEventListener("scroll", handleScroll);
+        window.addEventListener("scroll", handleScroll);
         return () => window.removeEventListener("scroll", handleScroll);
     }, [])
-    
-  return (
-      <header className={headerClass} id="home">
-        <div className={`header-top ${isActive ? "active" : ""}`}>
-            <a href="" className="logo">
-                <img src={logo} alt="lo+go"/>
-            </a>
-            <Nav navItems = {navItems} /> 
 
-            <div className="burger">
-                <MdOutlineMenu/>
-            </div>
-        </div>
-        <div className="header-bottom">
-{/* 
-            <div className="container">
-                <div className="header-content">
-                    <span className="header-sub">Hello,
-                        <span>I’m</span>
-                    </span>
-                    <h1 className="header-title">robert fox</h1>
-                    <p className="header-text">Professional Product Designer</p>
-                    <a href="" className="header-link btn-orange">let’s talk</a>
-                </div>
-                <div className="header-social social">
-                    {socialLinks.length ? (
-                        socialLinks.map((link,idx) => (
-                            <a href={link.url} key={idx} className="social-link">
-                                {link.icon}
-                            </a> 
-                        ))
-                    ):(
-                        <></>
-                    )}
+    return (
+        <header className={headerClass} id="home">
+            <div className={`header-top ${isActive ? "active" : ""}`}>
+                <a href="" className="logo">
+                    <img src={logo} alt="lo+go" />
+                </a>
+                <Nav navItems={navItems} />
+
+                <div className="burger">
+                    <MdOutlineMenu />
                 </div>
             </div>
-             */}
-        </div>
-    </header>
-  )
+            <div className="header-bottom">
+                {location.pathname === "/" ? (
+                    <div className="container">
+                        <div className="header-content">
+                            <span className="header-sub">Hello,
+                                <span>I’m</span>
+                            </span>
+                            <h1 className="header-title">robert fox</h1>
+                            <p className="header-text">Professional Product Designer</p>
+                            <a href="" className="header-link btn-orange">let’s talk</a>
+                        </div>
+                        <div className="header-social social">
+                            {socialLinks.length ? (
+                                socialLinks.map((link, idx) => (
+                                    <a href={link.url} key={idx} className="social-link">
+                                        {link.icon}
+                                    </a>
+                                ))
+                            ) : (
+                                <></>
+                            )}
+                        </div>
+                    </div>
+                ) : (
+                    <HeaderBottomPage/>
+                )}
+
+
+
+            </div>
+        </header>
+    )
 }
 
 
---
+++ src/components/header/HeaderBottomPage.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const HeaderBottomPage = ({title, subtitle}) => {
+  return (
+    <div className="header-bottom__about">
+            <div className="container">
+                <div className="header-content__about">
+
+                    <h1 className="header-title">{title}</h1>
+                    <span className="header-sub__about">home/
+                        <span>about</span>
+                    </span>
+                </div>
+
+            </div>
+        </div>
+  )
+}
+
+export default HeaderBottomPage
